12cdc5e733a53f2c225b2894a59fda229783a204
SergGrishkov <serg.grishkov@gmail.com>
2024-04-09 23:18:49 +0300

    Minor fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import { Link } from "react-router-dom";
 import css from "./Home.module.css";
 
 const Home = () => {
@@ -8,17 +7,7 @@ const Home = () => {
   return (
     <div className={css.mainBlock}>
       {!isLoggedIn && (
-        <p className={css.textLast}>
-          If you want to see a your phone numbers, please to{" "}
-          <Link className={css.link} to="/register">
-            Register
-          </Link>{" "}
-          or{" "}
-          <Link className={css.link} to="/login">
-            Login
-          </Link>{" "}
-          in this application.
-        </p>
+        <p className={css.textLast}>Register to use the phonebook.</p>
       )}
     </div>
   );
